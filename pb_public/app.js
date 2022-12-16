144c758599cb3e3bb3da9d069284c4710795055d
Leonardo Arenas <69521260+leonardoarenas@users.noreply.github.com>
2022-12-16 13:23:44 -0800

    Add files via upload

--- pb_public/app.js
+++ pb_public/app.js
@@ -5,8 +5,42 @@ console.log(pb.authStore.isValid);
 const signOutBtn = document.querySelector('button');
 
 signOutBtn.addEventListener("click", (event) => {
-    pb.authStore.clear()
+    pb.authStore.clear()})
+
+const form = document.getElementById('add-form');
+const todoList = document.getElementById('todo-list');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+	
+  const task = document.getElementById('task').value;
+  const li = document.createElement('li');
+  li.innerHTML = `<span>${task}</span><button>Delete</button>`;
+  todoList.appendChild(li);
+  const record = pb.collection('items').create({
+  	text: task,
+  	user: pb.authStore.baseModel.id,
+  	done: true
+  });
+  form.reset();
+  
+});
+
+todoList.addEventListener('click', e => {
+  if (e.target.tagName === 'BUTTON') {
+    const li = e.target.parentElement;
+    todoList.removeChild(li);
+  }
+});
 
 
-    window.location.href = "index.html";
-})
+
+todoList.addEventListener('click', e => {
+  if (e.target.tagName === 'SPAN') {
+    e.target.classList.toggle('completed');
+  }
+  window.location.href = "index.html";
+
+});
+
+    
